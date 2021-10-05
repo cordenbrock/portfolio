40f913ef6b1d2c514c550b13a87927ec0bcb023e
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-04 23:51:47 -0700

    Add feature to allow user to copy email to clipboard via clickable icon

---
+++ components/Contact/Email/index.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { AiOutlineMail } from "react-icons/ai";
+
+const Email = () => {
+  const [copySuccessMessage, setCopySuccessMessage] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const email = "cordenbrock@gmail.com";
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCopySuccessMessage("");
+    }, 5000);
+  }, [copySuccessMessage]);
+
+  function copyEmail() {
+    navigator.clipboard.writeText(email);
+    setCopySuccessMessage("Copied to Clipboard!");
+    setInstructions("");
+  }
+
+  function showInstruction() {
+    if (copySuccessMessage) {
+      return;
+    }
+    setInstructions(`${email}`);
+  }
+
+  function hideInstruction() {
+    setInstructions("");
+  }
+
+  return (
+    <nav>
+      <AiOutlineMail
+        onClick={copyEmail}
+        onMouseOver={showInstruction}
+        onMouseOut={hideInstruction}
+      />
+      <div className="absolute w-48 p-2 text-white transform md:-translate-x-20">
+        <p className="text-xs text-center">
+          {copySuccessMessage} {instructions}
+        </p>
+      </div>
+    </nav>
+  );
+};
+
+export default Email;
