f66a081e6503becf2cacb6756ae71ac538ea24b0
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-04 23:50:06 -0700

    Build out footer comp

--- components/Formatting/Footer/index.tsx
+++ components/Formatting/Footer/index.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { GiSasquatch } from "react-icons/gi";
+import { FaTree } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-      <footer className="mx-auto text-center bg-gray-900 text-md">
-          <h1>copyright stuff</h1>
-      </footer>
+    <footer className="fixed bottom-0 w-full mx-auto">
+      <div className="flex items-center justify-center text-xl text-gray-600 transform -translate-x-6">
+        <p className="text-black">Something&apos;s</p>
+        <FaTree />
+        <GiSasquatch />
+        <FaTree />
+        <p className="text-black"> Afoot!</p>
+      </div>
+    </footer>
   );
 };
