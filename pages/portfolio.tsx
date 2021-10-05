218c0d8e70f533d816b834aff9c4f1fd0d03afa6
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-04 23:40:33 -0700

    Template out page with basic cardcomponent--deconstruct and map through later

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,8 +1,15 @@
-import Card from "@components/Portfolio/Card";
+import Card from "@Portfolio/Card";
 
 export default function Portfolio() {
   return (
-    <main className="flex items-center justify-center h-full">      
+    <main className="flex flex-col justify-center h-full mx-auto max-w-7xl">
+      <div className="flex items-center justify-center w-full">
+        <hr className="w-1/3 m-8" />
+        <h1 className="font-extrabold text-black text-7xl black-outline">
+          PORTFOLIO
+        </h1>
+        <hr className="w-1/3 m-8" />
+      </div>
       <Card />
     </main>
   );
