95cfdafcb7388feee0c78e42ce7b72392ab96a5f
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-04 23:34:29 -0700

    Add custom animation configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,91 @@
 module.exports = {
-  purge: ['./pages/**/*.{tsx}', './components/**/*.{tsx}'],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./helpers/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundImage: {
-        'test1': "url('/Desert-bg.jpg')",
-        'test2': "linear-gradient(93deg, rgba(238,225,255,1) 0%, rgba(254,253,255,1) 71%, rgba(235,235,235,1) 100%)",
-        'm-u': "url('/m-u.png')",
-        'paro': "url('/paro.png')",
-        'exchangerator': "url('/exchangerator.png')"
+    extend: {
+      backgroundImage: {
+        utah: "url('/utah.jpg')",
+        "m-u": "url('/m-u.png')",
+        paro: "url('/paro.png')",
+        exchangerator: "url('/exchangerator.png')",
+        "im-a-banana": "url('/Untitled.png')",
+      },
+      fontFamily: {
+        monoton: ["Monoton", "cursive"],
+        outline: ["Bungee", "cursive"],
+      },
+      colors: {
+        primary: "rgba(36,14,5,.25)",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        blink: {
+          from: {
+            opacity: "1",
+          },
+          to: {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+        "cursor": "blink 1s linear infinite forwards",
+      },
     },
-    fontFamily: {
-      'logo': ['Monoton', 'cursive'],
-      'outline': ['Bungee', 'cursive']
-    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
