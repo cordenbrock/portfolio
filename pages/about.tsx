9415d8166736740432274f2e4d5ef1505758289e
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-05 04:56:59 -0700

    Make better use of next.js image optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,19 +12,21 @@ const About = () => {
     <>
       <main className="flex flex-col items-center justify-center w-full h-full max-w-6xl px-8 mx-auto">
         <div className="transform translate-y-12">
-          <div className="flex items-center justify-center w-full max-w-4xl mx-auto mb-8">
+          {/* header */}
+          <div className="flex items-center justify-center w-full max-w-4xl mx-auto mb-8 animate-fade-in-down">
             <hr className="w-1/3 m-8" />
             <h1 className="text-lg font-extrabold text-black uppercase sm:text-xl md:text-2xl lg:text-3xl xl:text-5xl 2xl:text-8xl black-outline">
               About
             </h1>
             <hr className="w-1/3 m-8" />
           </div>
-
+          {/* card */}
           <div className="p-8 mx-auto border-2 bg-primary max-w-7xl rounded-xl">
             <div className="flex flex-wrap ">
+              {/* row-1 */}
               <div className="w-full pl-3 mb-6 sm:pr-10 md:w-2/3 md:mb-0">
-                <h3 className="my-4 text-center sm:text-xl md:text-2xl font-outline black-outline">
-                  T h e &nbsp; W h o / W h a t / W h e r e / W h e n / W h y
+                <h3 className="my-4 sm:text-xl md:text-4xl font-outline black-outline">
+                  Profile
                 </h3>
                 <hr />
                 <br />
@@ -34,28 +36,28 @@ const About = () => {
                   </p>
                   <br />
                   <p>
-                    Originally from the <strong>Midwest</strong>, now hailing
-                    from the great <strong>PNW</strong>. First generation
-                    college-grad of humanities <em>turned</em> teacher of
-                    mathematics, <em>turned </em>
+                    Originally from the good ol&apos; <strong>Midwest</strong>,
+                    now hailing from the great <strong>PNW</strong>. First
+                    generation college-grad humanities major <em>turned</em>{" "}
+                    math teacher, <em>turned </em>
                     backpacking nomad, <em>turned </em> field service
                     technician, <em>turned </em>
                     <strong>self-taught developer </strong> with a coding
-                    bootcamp and{" "}
-                    <strong>professional web-dev experience</strong> now under
+                    bootcamp and <strong>professional web-dev experience</strong> now under
                     his belt.
                   </p>
                   <br />
-                  Has a knack for quickly picking up new skills and adapting to
-                  new environments. Communicates effectively. Detail-oriented.
-                  Team-player. Driven. An endless well of curiosity. And
+                  <p>Has a knack for quickly picking up new skills and adapting to
+                  new environments. Perceptive problem-solver. Detail-oriented. Team-player. Communicates effectively.
+                  An endless well of curiosity. Driven. And
                   contrary to popular belief, <em>not actually a banana.</em>
+                  </p>
                 </div>
               </div>
-
+              {/* row-2 */}
               <div className="flex flex-col items-center justify-center w-full px-3 mb-6 md:border-l-2 md:w-1/3 md:mb-0">
                 <h3
-                  className={`tracking-widest mb-2 text-lg text-white font-monoton ${
+                  className={`tracking-widest mb-2 text-lg text-white font-monoton  duration-500 hover:text-purple-200 ${
                     !isBananaUp ? "rotate-180" : null
                   }`}
                   onClick={flip}
@@ -70,26 +72,29 @@ const About = () => {
                 </h3>
                 <hr className="w-2/3 mb-4" />
                 <div
-                  className={`flex justify-end p-6 ease-out duration-1000 transform w-2/3 h-auto bg-black rounded-full border-white border-2 cursor-pointer ${
+                  className={`flex justify-end p-6 w-2/3 h-auto bg-black rounded-full border-white border-2 transform ease-in-out  duration-1000 hover:border-purple-200 cursor-pointer ${
                     !isBananaUp ? "rotate-180" : null
                   }`}
+                  onClick={flip}
                 >
                   <Image
                     src="/im-a-banana.png"
                     alt="self-portrait"
                     width={200}
                     height={200}
-                    onClick={flip}
+                    className={`flex justify-end p-6 w-2/3 h-auto bg-black rounded-full border-white border-2 transform ease-in-out duration-1000 hover:border-purple-300 cursor-pointer animate-fade-in-up ${
+                      !isBananaUp ? "rotate-180" : null
+                    }`}
                   />
                 </div>
                 <hr className="w-2/3 mt-4" />
-                <h3 className="mt-2 text-lg tracking-widest text-white font-monoton">
+                <h3 className="mt-2 text-lg tracking-widest text-white duration-500 font-monoton hover:text-purple-200 ">
                   <a
                     href="/resume.pdf"
                     target="_blank"
                     rel="noreferrer noopener"
                   >
-                    Résumé
+                    Self-Résumé
                   </a>
                 </h3>
               </div>
@@ -102,18 +107,3 @@ const About = () => {
 };
 
 export default About;
-
-{
-  /* <div className="flex justify-evenly">
-  <div className="flex justify-start">
-    <h3 className="my-4 text-3xl font-extrabold font-outline black-outline">
-      T e c h &nbsp; S t a c k
-    </h3>
-  </div>
-  <div className="flex">
-    <h3 className="my-4 text-3xl font-extrabold font-outline black-outline">
-      T e c h &nbsp; V i s i o n
-    </h3>
-  </div>
-</div>; */
-}
