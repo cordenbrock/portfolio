535b749b78541b89f1c3bc684af9e09bb02ab945
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-08 03:42:01 -0700

    Make more visible by adding bg and outline to form element

--- components/Contact/ContactForm/index.tsx
+++ components/Contact/ContactForm/index.tsx
@@ -34,9 +34,9 @@ const ContactForm = () => {
     <>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="max-w-4xl px-4 mx-auto h-72 animate-fade-in-up"
+        className="w-5/6 max-w-4xl p-8 mx-auto border-2 rounded-lg animate-fade-in-up bg-primary"
       >
-        <div className="flex flex-wrap mb-6 -mx-3">
+        <div className="flex flex-wrap mb-4 -mx-3">
           <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
             <label
               className="block mb-2 text-xs font-bold tracking-wide text-gray-400 uppercase"
@@ -86,7 +86,7 @@ const ContactForm = () => {
           </div>
         </div>
 
-        <div className="flex flex-wrap mb-6 -mx-3">
+        <div className="flex flex-wrap mb-4 -mx-3">
           <div className="w-full px-3">
             <label
               className="block mb-2 text-xs font-bold tracking-wide text-gray-400 uppercase"
@@ -112,84 +112,19 @@ const ContactForm = () => {
         </div>
 
         <div className="flex justify-between">
-            <button
-              className="px-4 py-2 font-bold text-gray-900 bg-gray-400 rounded shadow flex-start hover:bg-gray-900 hover:text-gray-400 focus:shadow-outline focus:outline-none"
-              type="submit"
-            >
-              Send
-            </button>
-            {isSubmitSuccessful && !isDirty && (
-              <span className="py-2 pl-4 text-center text-green-600 flex-end">
-                Message Sent!
-              </span>
-            )}
-        </div>
-      </form>
-
-      {/* <form className="w-2/3 p-4 bg-gray-900 rounded-md">
-      <div className="flex flex-wrap mb-6 -mx-3">
-        <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
-          <label
-            className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
-            htmlFor="grid-first-name"
-          >
-            First Name
-          </label>
-          <input
-            className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-red-500 rounded appearance-none focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder="Jane"
-          />
-          <p className="text-xs italic text-red-500">
-            Please fill out this field.
-          </p>
-        </div>
-
-        <div className="w-full px-3 md:w-1/2">
-          <label
-            className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
-            htmlFor="grid-last-name"
-          >
-            Last Name
-          </label>
-          <input
-            className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="text"
-            placeholder="Doe"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-wrap mb-6 -mx-3">
-        <div className="w-full px-3">
-          <label
-            className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
-            htmlFor="grid-password"
-          >
-            Message
-          </label>
-          <textarea
-            cols={30}
-            rows={2} 
-            className="block w-full h-48 px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-            id="message"
-          ></textarea>
-        </div>
-      </div>
-
-      <div className="md:flex md:items-center">
-        <div className="md:w-1/3">
           <button
-            className="px-4 py-2 font-bold text-white bg-teal-400 rounded shadow hover:bg-teal-400 focus:shadow-outline focus:outline-none"
-            type="button"
+            className="px-4 py-2 font-bold text-gray-900 bg-gray-400 rounded shadow flex-start hover:bg-gray-900 hover:text-gray-400 focus:shadow-outline focus:outline-none"
+            type="submit"
           >
             Send
           </button>
+          {isSubmitSuccessful && !isDirty && (
+            <span className="py-2 pl-4 text-center text-green-600 flex-end">
+              Message Sent!
+            </span>
+          )}
         </div>
-      </div>
-    </form> */}
+      </form>
     </>
   );
 };
