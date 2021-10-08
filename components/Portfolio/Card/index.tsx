f892bae83e9cbf7e057554816e94e9577de05f1a
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-08 03:37:43 -0700

    Refactor to represent/render one generic card

--- components/Portfolio/Card/index.tsx
+++ components/Portfolio/Card/index.tsx
@@ -1,57 +1,22 @@
-const Card = () => {
+const Card = ({ name, href, img }) => {
   return (
-    <section className="px-4 mx-auto mb-12 sm:px-6 animate-fade-in-up">
-        <div className="grid grid-cols-1 mt-6 md:grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-8">
-          <div className="relative w-full h-64 overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg bg-m-u group hover:shadow-2xl">
-            <div className="absolute inset-0 transition duration-300 ease-in-out bg-purple-100 bg-opacity-90 group-hover:opacity-10"></div>
-            <div className="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
-              <h3 className="text-center">
-                <a
-                  className="text-2xl font-bold text-center text-white text-shadow"
-                  href="https://www.musica-universalis.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <span className="absolute inset-0 transform hover:-translate-y-10"></span>
-                  Musica Universalis
-                </a>
-              </h3>
-            </div>
-          </div>
-          <div className="relative w-full h-64 overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg group hover:shadow-2xl bg-paro">
-            <div className="absolute inset-0 transition duration-300 ease-in-out bg-gray-900 bg-opacity-90 group-hover:opacity-10"></div>
-            <div className="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
-              <h3 className="text-center">
-                <a
-                  className="text-2xl font-bold text-center text-white text-shadow"
-                  href="http://paro-for-president.herokuapp.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <span className="absolute inset-0"></span>
-                  Paro
-                </a>
-              </h3>
-            </div>
-          </div>
-          <div className="relative w-full h-64 overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg group hover:shadow-2xl bg-exchangerator">
-            <div className="absolute inset-0 transition duration-300 ease-in-out bg-green-50 bg-opacity-90 group-hover:opacity-10"></div>
-            <div className="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
-              <h3 className="text-center">
-                <a
-                  className="text-2xl font-bold text-center text-white text-shadow"
-                  href="https://exchangerator.herokuapp.com/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <span className="absolute inset-0"></span>
-                  Exchangerator{" "}
-                </a>
-              </h3>
-            </div>
+        <div className={`relative w-full h-64 overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded shadow-lg bg-${img} group hover:shadow-2xl`}>
+          <div className="absolute inset-0 transition duration-300 ease-in-out bg-purple-100 bg-opacity-90 group-hover:opacity-10"></div>
+          <div className="relative flex items-center justify-center w-full h-full px-4 duration-1000 transform sm:px-6 lg:px-4 hover:translate-y-16">
+            <h3 className="text-center">
+              <a
+                className="text-2xl font-bold text-center text-white text-shadow"
+                href={href}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <span className="absolute inset-0"></span>
+                {name}
+              </a>
+            </h3>
           </div>
         </div>
-    </section>
+
   );
 };
 
