198ab6064756809659dae335aab8868d01083651
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-08 03:34:01 -0700

    Remove unnecessary div

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,8 @@ import Landing from "@Home/Landing";
 
 export default function Home() {
   return (
-    <>
-      <main className="flex items-center justify-center h-full">
-        <Landing />
-      </main>
-    </>
+    <main className="flex items-center justify-center h-full">
+      <Landing />
+    </main>
   );
 }
