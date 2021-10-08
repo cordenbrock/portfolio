64951b35e7084a2b174e48f15dfb0869c699a1bf
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-08 03:35:28 -0700

    Refactor with typescript and add content object

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,18 +1,50 @@
+import Header from "@Formatting/Header";
 import Card from "@Portfolio/Card";
 
+type Content = {
+  pageTitle: string;
+  apps: App[];
+};
+
+type App = {
+    name: string;
+    href: string;
+    img: string;
+}
+
 export default function Portfolio() {
+  const content: Content = {
+    pageTitle: "Portfolio",
+    apps: [
+      {
+        name: "Musica Universalis",
+        href: "https://www.musica-universalis.com/",
+        img: "m-u",
+      },
+      {
+        name: "Paro",
+        href: "http://paro-for-president.herokuapp.com/",
+        img: "paro",
+      },
+      {
+        name: "Exchangerator",
+        href: "https://exchangerator.herokuapp.com/",
+        img: "exchangerator",
+      },
+    ],
+  };
+
   return (
-    <main className="flex flex-col items-center justify-center w-full h-full max-w-6xl px-8 mx-auto">
-      <div className="w-full transform translate-y-12">
-        <div className="flex items-center justify-center w-full mx-auto mb-8 animate-fade-in-down">
-          <hr className="w-1/3 m-8" />
-          <h1 className="text-lg font-extrabold text-black uppercase sm:text-xl md:text-2xl lg:text-3xl xl:text-5xl 2xl:text-8xl black-outline">
-            Portfolio
-          </h1>
-          <hr className="w-1/3 m-8" />
+    <main className="flex flex-col items-center justify-center h-full mx-auto">
+      <div className="h-24"></div>
+      <Header pageTitle={content.pageTitle} />
+      <section className="p-8 mx-auto border-2 rounded-xl bg-primary animate-fade-in-up">
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-8">
+          {content.apps.map((app, index) => (
+            <Card key={index} {...app} />
+          ))}
         </div>
-        <Card />
-      </div>
+      </section>
     </main>
   );
 }
