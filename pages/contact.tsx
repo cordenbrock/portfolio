cc2a8e65fbf33a54f8c780b15719efee808da35a
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-08 03:36:12 -0700

    Refactor with typescript, add content object, abstract header elements to component

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,16 +1,20 @@
+import Header from "@Formatting/Header";
 import ContactForm from "@Contact/ContactForm";
 
+type Content = {
+  pageTitle: string;
+};
+
 const Contact = () => {
+  const content: Content = {
+    pageTitle: "Contact",
+  };
+
   return (
-    <main className="flex flex-col items-center justify-center h-full mx-auto max-w-7xl">
-      <div className="flex items-center justify-center w-full h-48 max-w-4xl mx-auto rounded-md animate-fade-in-down">
-        <hr className="w-1/3 m-8" />
-        <h1 className="text-3xl font-extrabold text-black uppercase sm:text-4xl md:text-5xl lg:text-7xl black-outline">
-          Contact
-        </h1>
-        <hr className="w-1/3 m-8" />
-      </div>
-        <ContactForm />
+    <main className="flex flex-col items-center justify-center h-full mx-auto">
+      <div className="h-24"></div>
+      <Header pageTitle={content.pageTitle} />
+      <ContactForm />
     </main>
   );
 };
