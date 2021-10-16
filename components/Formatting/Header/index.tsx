0d38c9743153536d312415f79a56e003a35a5176
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-15 17:53:14 -0700

    Change font style to bungee-outline

--- components/Formatting/Header/index.tsx
+++ components/Formatting/Header/index.tsx
@@ -1,8 +1,8 @@
 const Header = ({ pageTitle }) => {
   return (
-    <div className="flex items-center justify-center w-full max-w-5xl mx-auto mb-8 animate-fade-in-down">
+    <div className="flex items-center justify-center w-full max-w-6xl mx-auto mb-8 mix-blend-screen animate-fade-in-down">
       <hr className="w-1/3 m-8" />
-      <h1 className="text-3xl font-extrabold text-black uppercase sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-8xl white-outline">
+      <h1 className="text-3xl tracking-widest uppercase font-outline sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-8xl white-outline">
         {pageTitle}
       </h1>
       <hr className="w-1/3 m-8" />
