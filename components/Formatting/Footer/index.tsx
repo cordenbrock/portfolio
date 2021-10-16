8ba26513deea97a2d442239971990bef053477df
Cory Nordenbrock <cordenbrock@gmail.com>
2021-10-15 17:52:15 -0700

    Make hidden text actually transparent

--- components/Formatting/Footer/index.tsx
+++ components/Formatting/Footer/index.tsx
@@ -5,11 +5,11 @@ export const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full mx-auto">
       <div className="flex items-center justify-center text-xl text-gray-600 transform -translate-x-6">
-        <p className="text-black">Something&apos;s</p>
+        <p className="text-transparent">something&apos;s</p>
         <FaTree />
         <GiSasquatch />
         <FaTree />
-        <p className="text-black"> Afoot!</p>
+        <p className="text-transparent">afoot!</p>
       </div>
     </footer>
   );
